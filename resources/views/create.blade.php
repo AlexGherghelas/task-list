bf57bf996b18e1199a48e6768a614da426e7ac85
AlexGherghelas <alex@travpromobile.com>
2024-10-09 15:20:25 +0200

    keeping old values

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -17,15 +17,15 @@
             <label for="title">
                 Title
             </label>
-            <input text="text" name="title" id="title"/>
+            <input text="text" name="title" id="title" value="{{old('title')}}"/>
             @error('title')
             <p class="error-message">{{$message}}</p>
             @enderror
         </div>
 
         <div>
-            <label>Description</label>
-            <textarea name="description" id="description" rows="5"></textarea>
+            <label for="description">Description</label>
+            <textarea name="description" id="description" rows="5" >{{old('description')}}</textarea>
             @error('description')
             <p class="error-message">{{$message}}</p>
             @enderror
@@ -33,7 +33,7 @@
 
         <div>
             <label>Long Description</label>
-            <textarea name="long_description" id="long_description" rows="10"></textarea>
+            <textarea name="long_description" id="long_description" rows="10">{{old('long_description')}}</textarea>
             @error('long_description')
            <p class="error-message">{{$message}}</p>
             @enderror
@@ -44,3 +44,5 @@
         </div>
 
     </form>
+
+    @endsection
