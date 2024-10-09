5cfabbe850a2a15acd650a8ee3e636cab991cd76
AlexGherghelas <alex@travpromobile.com>
2024-10-09 14:55:47 +0200

    add task & validation

--- routes/web.php
+++ routes/web.php
@@ -2,29 +2,39 @@
 
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Models\Task;
 
-class Task
-{
-  public function __construct(
-    public int $id,
-    public string $title,
-    public string $description,
-    public ?string $long_description,
-    public bool $completed,
-    public string $created_at,
-    public string $updated_at
-  ) {
-  }
-}
 
 Route::get('/', function () {
   return redirect() -> route('tasks.index');
 });
 
 Route::get('/tasks', function ()  {
-    return view('index', ['tasks' => \App\Models\Task::latest()->where('completed', true)->get()]);
+    return view('index', ['tasks' => Task::latest()->where('completed', true)->get()]);
 })->name('tasks.index');
 
+Route::view('/tasks/create', 'create') ->name('task.create');
+
 Route::get('/tasks/{id}', function ($id) {
- return view('show', ['task' => \App\Models\Task::findOrFail($id)]);
+ return view('show', ['task' => Task::findOrFail($id)]);
 }) ->name('tasks.show');
+
+Route::post('/tasks', function(Request $request) {
+  $data = $request->validate([
+    'title'=> 'required|max:255',
+    'description'=> 'required',
+    'long_description' => 'required'
+  ]);
+
+  $task = new Task();
+  $task->title = $data['title'];
+  $task->description = $data['description'];
+  $task->long_description = $data['long_description'];
+
+  $task->save();
+
+  return redirect() -> route('tasks.show', ['id' => $task->id])
+        ->with('success' , 'Task created successfully');
+
+})->name('tasks.store');
